e9192b3e0139283f26af0275e308ac1f21e27acd
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-02 23:13:21 +0530

    create a new SampleController without Annotaions

---
+++ src/main/java/com/conceptandcoding/springbootplayground/Controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.conceptandcoding.springbootplayground.Controller;
+
+public class SampleController {
+
+    public String getUserDetails() {
+        return "fetching and returning user details";
+    }
+
+    public String saveUserDetails() {
+        return "successfully saved user details";
+    }
+}
