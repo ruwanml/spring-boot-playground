0856a4ba45375fb5b2c7cff3dc753456149eb6ba
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-06 15:06:55 +0530

    Save a Java Object

--- src/main/java/com/conceptandcoding/springbootplayground/SpringBootPlaygroundApplication.java
+++ src/main/java/com/conceptandcoding/springbootplayground/SpringBootPlaygroundApplication.java
@@ -1,13 +1,37 @@
 package com.conceptandcoding.springbootplayground;
 
+import com.conceptandcoding.springbootplayground.dao.StudentDAO;
+import com.conceptandcoding.springbootplayground.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(SpringBootPlaygroundApplication.class, args); }
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object..");
+		Student tempStudent = new Student("Maal", "G", "maal@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student..");
+		studentDAO.save(tempStudent);
+
+		// display the id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
 	}
 
 }
---
+++ src/main/java/com/conceptandcoding/springbootplayground/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.conceptandcoding.springbootplayground.dao;
+
+import com.conceptandcoding.springbootplayground.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
