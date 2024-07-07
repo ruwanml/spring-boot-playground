2790c2a488a6640e35e4bd0a9fcaccdcba8d28ed
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-07 16:32:13 +0530

    Spring Rest Service - Student - Refactor the code by using PostConstruct annotaion

--- src/main/java/com/conceptandcoding/springbootplayground/rest/StudentRestController.java
+++ src/main/java/com/conceptandcoding/springbootplayground/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.conceptandcoding.springbootplayground.rest;
 
 import com.conceptandcoding.springbootplayground.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of student
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> studentList;
 
-        List<Student> studentList = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+        studentList = new ArrayList<>();
+
         studentList.add(new Student("Jon", "Da"));
         studentList.add(new Student("Kamla", "Haris"));
         studentList.add(new Student("Risi", "Sunk"));
+    }
 
+    // define endpoint for "/students" - return a list of student
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return studentList; // Jackson will convert List<Student> to JSON array
     }
 }
