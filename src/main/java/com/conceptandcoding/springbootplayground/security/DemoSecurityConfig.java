930761ea73d7a8fe67d05e1f8b09c98a1305b8e8
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-08 12:51:05 +0530

    Spring Boot- Rest API - Configuring Basic Security

---
+++ src/main/java/com/conceptandcoding/springbootplayground/security/DemoSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.conceptandcoding.springbootplayground.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        // define users
+
+        UserDetails jhon = User.builder()
+                .username("jhon")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(jhon, mary, susan);
+
+    }
+}
