c07cdad821dae4e739369738ad20967d6e299ad4
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-08 14:43:02 +0530

    Spring Boot- Rest API - Restrict Access Based on Roles

--- src/main/java/com/conceptandcoding/springbootplayground/security/DemoSecurityConfig.java
+++ src/main/java/com/conceptandcoding/springbootplayground/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.conceptandcoding.springbootplayground.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -34,5 +38,33 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(jhon, mary, susan);
 
+    }
+
+    // restrict access based on roles
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // setting up authorization rules to access end-points based on roles
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP basic authentication
+
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable CSRF - cross site request forgery
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
